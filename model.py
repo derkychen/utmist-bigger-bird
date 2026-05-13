273ff3c46541843606014460b89ddcd90cd3ea9c
Derek Chen <derekjchen@proton.me>
2026-05-13 18:42:18 +0200

    testing webhook

--- model.py
+++ model.py
@@ -1,14 +1,12 @@
 import torch
 import torch.nn as nn
 
-from transformers import (
-    BigBirdModel,
-    BigBirdConfig
-)
+from transformers import BigBirdModel, BigBirdConfig
 
 # For now, just running BigBird as it is
 config = BigBirdConfig()
 
 model = BigBirdModel(config)
 
-print(model) # test
+print(model)  # test
+
