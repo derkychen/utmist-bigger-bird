f2db59adbe6f58a707ebf8cd810661fb581a6797
hanDynasty0 <flyshark2006@gmail.com>
2026-05-12 23:32:22 -0400

    preliminary code, just loading a BigBird object by itself

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+from transformers import (
+    BigBirdModel,
+    BigBirdConfig
+)
+
+# For now, just running BigBird as it is
+
+config = BigBirdConfig()
+
+model = BigBirdModel(config)
+
+print(model)
