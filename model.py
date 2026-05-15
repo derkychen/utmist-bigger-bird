8e32350b81919244157699e2e0a9c6ed4748063e
hanDynasty0 <flyshark2006@gmail.com>
2026-05-15 16:55:08 -0400

    initialize bigger bird attention class, update gitignore

--- model.py
+++ model.py
@@ -1,15 +1,26 @@
 import torch
 import torch.nn as nn
 
-from transformers import (
-    BigBirdModel,
-    BigBirdConfig
-)
+# from transformers import (
+#     BigBirdModel,
+#     BigBirdConfig
+# )
 
 # For now, just running BigBird as it is
 
-config = BigBirdConfig()
+# config = BigBirdConfig()
 
-model = BigBirdModel(config)
+# model = BigBirdModel(config)
 
-print(model) # test
+# Check the class that BigBird's attention model lives in
+# print(type(model.encoder.layer[0].attention.self))
+
+from transformers.models.big_bird.modeling_big_bird import (
+    BigBirdBlockSparseAttention
+)
+
+class BiggerBirdAttention(BigBirdBlockSparseAttention):
+    def __init__(self, config):
+        super().__init__(config)
+    
+    
